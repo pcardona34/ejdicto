f5d7608ed7e7c219b695c72b9c537ce57db29bdd
Patrick Cardona <pcardona34@gmail.com>
2020-04-14 12:32:12 +0200

    Mise à jour : version 1.0.1

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-echo "Test before publishing..."
-
-echo "Serveur Web local (Python http)..."
-
-python3 -m http.server
--- vendors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Cleaning and minifying CSS : vendors..."
-echo "1) w3.css" && cleancss -o vendor/w3school/styles/w3.min.css vendor/w3school/styles/w3.css;
-echo "2) IcoMoon icons..." && cleancss -o vendor/icomoon/style.min.css vendor/icomoon/style.css;
-echo "Minifying JS..."
-echo "1) w3.js" && jsmin -o vendor/w3school/scripts/w3.min.js vendor/w3school/scripts/w3.js;
-echo "All is done !";
-
